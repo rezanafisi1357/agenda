775c4c8f4833f4e47e781555b968a13ebcf53d5a
rezanafisi1357 <nafisi.reza1357@gmail.com>
2026-08-19 13:28:32 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'agenda-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE_NAME; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      if(cached) return cached;
+      return fetch(event.request).then(function(response){
+        var copy = response.clone();
+        caches.open(CACHE_NAME).then(function(cache){
+          cache.put(event.request, copy);
+        });
+        return response;
+      }).catch(function(){
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
